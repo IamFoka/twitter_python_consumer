f3eb0148e4b80b7a7fa1c3c6e64a077234588623
Guilherme de Melo Barragam <guilherme@nautec.com.br>
2019-09-17 20:10:35 -0300

    Adds function to write tweets user info in text file

--- src/main.py
+++ src/main.py
@@ -3,18 +3,25 @@ import configparser
 import pandas as pd
 from loguru import logger
 
+
+def write_tweet(tweet, file):
+    line = f'{tweet.id},{tweet.user.screen_name},{tweet.user.location}\n'
+    file.write(line)
+
+file = open('data.txt', 'a')
+
 config = configparser.ConfigParser()
 config.read('credentials.ini')
 
 auth = tweepy.OAuthHandler(config['DEFAULT']['consumer_key'],config['DEFAULT']['consumer_secret'])
 auth.set_access_token(config['DEFAULT']['access_token'],config['DEFAULT']['access_token_secret'])
 
-search_words = "#marvel"
+search_words = "#python"
 
 api = tweepy.API(auth)
 tweets = tweepy.Cursor(api.search,
                         q = search_words,
-                        lang = 'pt',
+                        lang = 'en',
                         ).items()
 
 # users_locs = [[tweet.user.screen_name, tweet.user.location] for tweet in tweets]
@@ -25,6 +32,6 @@ tweets = tweepy.Cursor(api.search,
 n = 0
 
 for tweet in tweets:
-    logger.info(n)
-    logger.info(tweet.created_at)
-    n += 1
+    write_tweet(tweet, file)
+
+file.close()
